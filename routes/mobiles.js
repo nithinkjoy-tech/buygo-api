428c2918e99d55da5a7ea8ac9f5b467d8c7770d5
Nithin K Joy <nithinjoy121212@gmail.com>
2021-04-28 18:39:38 +0530

    changed

--- routes/mobiles.js
+++ routes/mobiles.js
@@ -13,7 +13,7 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  let data = req.body
+  let data = req.body 
   let { error } = validate(data);
   if (error) return res.status(400).send(error);
   let mobiles = new Mobile(data);
